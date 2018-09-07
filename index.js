3bdc268f84b0f2e1fd7aa0a7ae67d23c0f21f2cd
Noah Gil <xlash19o13@gmail.com>
2018-09-06 23:30:38 -0400

    Added mobile support

--- index.js
+++ index.js
@@ -186,6 +186,41 @@ const handleKeyUp = function(e){
 	keys[e.keyCode] = false;
 }
 
+const touchStart = function(evt){
+	var changed = evt.changedTouches;
+	for (var i=0; i<changed.length; i++){
+		var touch = changed[i];
+		handleTouch(touch);
+	}
+}
+
+const touchMove = function(evt){
+	var changed = evt.changedTouches;
+	for (var i=0; i<changed.length; i++){
+		var touch = changed[i];
+		handleTouch(touch);
+	}
+}
+
+const handleTouch = function(touch, down=true){
+	var x = touch.pageX - game.canvasX;
+	var y = touch.pageY - game.canvasY;
+	var point = new paper.Point(x, y);
+
+	keys[KEY.LEFT] = game.padLeft.contains(point);
+	keys[KEY.RIGHT] = game.padRight.contains(point);
+	keys[KEY.UP] = game.padUp.contains(point);
+	keys[KEY.DOWN] = game.padDown.contains(point);
+	keys[KEY.SPACE] = game.padShoot.contains(point);
+}
+
+const touchEnd = function(evt){
+	var changed = evt.changedTouches;
+	for (var i=0; i<changed.length; i++){
+		handleTouch(touch, false);
+	}
+}
+
 class Game {
 
 	constructor(canvas){
@@ -197,10 +232,29 @@ class Game {
 		this.bullets = [];
 		this.meteors = [];
 		this.score = 0;
+		this.isMobile = window.mobilecheck();
+		this.canvasX = canvas.getBoundingClientRect().left;
+		this.canvasY = canvas.getBoundingClientRect().top;
 
 		this.scoreItem = new paper.PointText(new paper.Point(20, 20));
 		this.scoreItem.strokeColor = "white";
 		this.scoreItem.fontSize = 12;
+
+		this.padLeft = new paper.Path.Rectangle(2, canvas.height-104, 50, 50);
+		this.padLeft.strokeColor = "white";
+		this.padLeft.strokeWidth = 1;
+		this.padDown = new paper.Path.Rectangle(53, canvas.height-52, 50, 50);
+		this.padDown.strokeColor = "white";
+		this.padDown.strokeWidth = 1;
+		this.padRight = new paper.Path.Rectangle(104, canvas.height-104, 50, 50);
+		this.padRight.strokeColor = "white";
+		this.padRight.strokeWidth = 1;
+		this.padUp = new paper.Path.Rectangle(53, canvas.height-154, 50, 50);
+		this.padUp.strokeColor = "white";
+		this.padUp.strokeWidth = 1;
+		this.padShoot = new paper.Path.Circle(new paper.Point(canvas.width-50, canvas.height-50), 48);
+		this.padShoot.strokeColor = "white"
+		this.padShoot.strokeWidth = 1;
 
 		if(getCookie("highscore") == null){
 			console.log("No high score exists. Adding new one.");
@@ -209,9 +263,14 @@ class Game {
 	}
 
 	start(){
-		addEventListener("keydown", handleKeyDown(this.ship), false);
-		addEventListener("keyup", handleKeyUp, false);
-		
+		if(this.isMobile){
+			this.canvas.addEventListener("touchstart", touchStart, false);
+			this.canvas.addEventListener("touchmove", touchMove, false);
+			this.canvas.addEventListener("touchend", touchEnd, false);
+		}else{
+			addEventListener("keydown", handleKeyDown(this.ship), false);
+			addEventListener("keyup", handleKeyUp, false);
+		}
 		this.loop();
 	}
 
@@ -335,6 +394,12 @@ window.addEventListener('load', function(e) {
   }, false);
 
 }, false);
+
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
 
 let canvas = document.getElementById("screen");
 let game = new Game(canvas);
